9936e977fc079357d4d91e9e6bb2e074c8ff19b0
Marwan Mohamed <marawan.mohamed.refaei@gmail.com>
2022-09-16 10:20:47 +0200

    add circleci config2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React from circle ci
+          React from circle CI
         </p>
         <a
           className="App-link"
